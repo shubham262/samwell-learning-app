d489df56642dc3c50bc0891142c1684677c8db95
Shubham PC <shubhampc@Shubhams-MacBook-Air.local>
2025-04-03 14:47:05 +0530

    landing page changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,46 @@
+"use client";
+
 import Image from "next/image";
 import styles from "../assets/styles/home.module.scss";
-import { memo } from "react";
+import { memo, useState, useCallback } from "react";
 import Header from "@/views/components/home/Header";
 import Footer from "@/views/components/home/Footer";
-// import Image from "next/image";
+import book from "../assets/svg/book.svg";
 import samWell from "@/assets/icons/container.png";
 const Home = () => {
+  const [info, setInfo] = useState({
+    userQuery: "",
+  });
+
+  const onQueryChange = useCallback(
+    (e) => {
+      setInfo((prev) => ({ ...prev, userQuery: e.target.value }));
+    },
+    [info]
+  );
   return (
     <div className={styles.homePageContainer}>
       <Header />
       <div className={styles.homePageContent}>
+        <div className=""></div>
         <Image src={samWell} />
         <span className={styles.supprtingText}>
           Explore how Samwell can help you learn what you need, effortlessly.
         </span>
-        <div className=""></div>
+        <div className={styles.tutorContainer}>
+          <div className={styles.tutorHeader}>
+            <Image src={book} alt="book" />
+            <span className={styles.tutorTitle}>AI tutor</span>
+          </div>
+          <div className={styles.studyInput}>
+            <textarea
+              placeholder="Type what you want to study..."
+              onChange={onQueryChange}
+              value={info.userQuery}
+            />
+            <button className={styles.startButton}>Start Studying</button>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
